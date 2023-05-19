b70aade8abcee96217562e75b8779ff5f49d42c3
Alan Paiva <allanpaiva@mail.io>
2023-05-19 19:18:16 -0300

    atomic func

--- concurrency/main.go
+++ concurrency/main.go
@@ -4,18 +4,15 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"sync"
+	"sync/atomic"
 	"time"
 )
 
 var number uint64 = 0
 
 func main() {
-	m := sync.Mutex{}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		m.Lock()
-		number++
-		m.Unlock()
+		atomic.AddUint64(&number, 1)
 		time.Sleep(300 * time.Millisecond)
 		w.Write([]byte(fmt.Sprintf("Acesso a pagina %d vezes", number)))
 		w.Header().Set("Content-Type", "application/json")
